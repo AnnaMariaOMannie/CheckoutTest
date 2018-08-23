4a1c6e73cbb5cb9da53c132a9b8158ddd773737f
AniaMaria <amasapps2nv@gmail.com>
2018-08-23 17:05:46 +0100

    First Step Done

--- CheckoutTest/ViewController.swift
+++ CheckoutTest/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let apple:Double = 0.60
+    let orange:Double = 0.25
+
+    var runningCost:Double = 0
+
+
+    @IBOutlet weak var totalCostLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buyAppleBtnPressed(_ sender: Any) {
 
+        runningCost += apple
+        totalCostLbl.text = String(runningCost)
+    }
+    
+    @IBAction func buyOrangeBtnPressed(_ sender: Any) {
+        runningCost += orange
+        totalCostLbl.text = String(runningCost)
+    
+    }
 }
 
